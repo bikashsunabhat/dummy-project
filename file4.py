59e1770436c9e59cc9728159306cdbc743bb597b
bikashsunabhat <47473723+bikashsunabhat@users.noreply.github.com>
2019-02-16 16:39:08 +0545

    added new file from myside

---
+++ file4.py
@@ -0,0 +1,50 @@
+print("hello")
+print("hello\\hi")
+print("hi \neverybody")
+print("to see\"python\"")
+print("this is doublebackslash\\\\")
+print("hello\t nepal")
+#escape  above 
+"""nepal comment"""
+# print("shortcut comment ctr+/")
+print("line\\nline")
+x=2.5
+print(type(x))
+print(isinstance(x,float))
+y=2
+print(isinstance(y,int))
+print("\\\"nepal\\\"")
+print("/\\/\\/\\ this is mountain")
+print("#\\\" and \\n and also \\t also \\\'")
+print(r"this is \nnormal text")
+print(r"this is \\\t mormal text ")
+# print emoji exercise
+print("\U0001F60D")
+print("\U0001F49B")
+# calculation of numbers
+print(2+4)
+print(4/2 ," is floating point division")
+print(4//2 ," is integer  point division")
+print(2**3,"is power calculation")
+print(2**.5,"is power calculation")
+# round up
+print(round(2**.53,4),"is power calculation")
+# precendence rule
+# *,/,//,% left to right and exponentation right to left
+#string concation
+f_name="Ram"
+l_name="Karki"
+full_name=f_name+" "+l_name
+print(full_name)
+print((f_name +" " +l_name)*2)
+# user input
+name=input("enter your name\n")
+print("hello " +name)
+number_one=int(input("enter first number:\t"))
+number_two=int(input("enter number:\t"))
+total=number_one+number_two
+print("total sum is "+str(total))
+
+
+
+	
